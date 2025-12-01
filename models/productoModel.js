9cc24133d11244a8edbec5fa795063b6f0aad6c1
carlo <carlosvibot14@gmail.com>
2025-12-01 01:55:26 -0600

    add model pizzas restaurant

--- models/productoModel.js
+++ models/productoModel.js
@@ -11,7 +11,7 @@ const Producto = sequelize.define('productos', {
 
     pizzeria_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
             model: Pizzeria,
             key: 'id'
